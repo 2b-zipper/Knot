9d0fd3d72326d7343f289311ffa18177d6e43ae1
2b-zipper <119087427+2b-zipper@users.noreply.github.com>
2026-08-20 13:14:39 +0900

    26.13.0に対応

--- app/src/main/java/app/zipper/knot/LineVersion.java
+++ app/src/main/java/app/zipper/knot/LineVersion.java
@@ -555,6 +555,7 @@ public class LineVersion {
     VERSION_TABLE.put("26.10.0", app.zipper.knot.versions.Version26100.create());
     VERSION_TABLE.put("26.10.1", app.zipper.knot.versions.Version26101.create());
     VERSION_TABLE.put("26.11.0", app.zipper.knot.versions.Version26110.create());
+    VERSION_TABLE.put("26.13.0", app.zipper.knot.versions.Version26130.create());
   }
 
   private static volatile Config cachedConfig = null;
